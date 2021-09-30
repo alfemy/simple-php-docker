a1e3b0fc7645400190688aa77b8412a94c9f588a
Aleksandr Prikhozhdenko <aprikhozhdenko@plesk.com>
2021-09-30 15:02:16 +0700

    another one long ocmefsdddfffjffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffddddddd dddddddddddddddkkkkkkkkkkkkkkkdddddddddddddddddkkk

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Hello, World from Docker! <br>";
+echo "Hello, World from Docker ruuning at ECS!<br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png"><br>';
 
 while (list($var,$value) = each ($_ENV)) {
