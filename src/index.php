fb46182d5ca55eeb9ec68ea172860c224436dc77
Aleksandr Prikhozhdenko <aprikhozhdenko@plesk.com>
2021-10-10 21:09:56 +0700

    Display of result of mount command

--- src/index.php
+++ src/index.php
@@ -1,10 +1,8 @@
 <?php
 
-echo "Hello, World from Docker ruuning at ECS!<br>";
+echo "Hello, World from Docker ruuning at !<br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png"><br>';
-
-while (list($var,$value) = each ($_ENV)) {
-    echo "$var => $value <br />";
-}
-
+echo "<h1>Show mounts inside of container:</h1>";
+$output = shell_exec('mount');
+echo "<pre>$output</pre>";
 ?>
