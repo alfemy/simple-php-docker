b93a472dd507d86ff7e3de5b75af04e0a75a097a
Aleksandr Prikhozhdenko <aprikhozhdenko@plesk.com>
2021-10-10 21:49:26 +0700

    Add file into mounted NFS dir and print the dir

--- src/index.php
+++ src/index.php
@@ -2,6 +2,26 @@
 
 echo "Hello, World from Docker ruuning at !<br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png"><br>';
+shell_exec('touch /mnt/data/file');
+shell_exec("head -c 30M </dev/urandom >/mnt/data/myfile");
+
+
+
+echo '<h1>Show contains of /mnt/data/</h1>';
+$lsOutput = shell_exec('ls -lh /mnt/data');
+echo "<pre>$lsOutput</pre>";
+
+shell_exec('touch /tmp/testfile');
+
+echo '<h1>Show contains of /tmp/</h1>';
+$lsOutput = shell_exec('ls -lh /tmp/');
+echo "<pre>$lsOutput</pre>";
+
+
+echo '<h1>Show contains of /mnt/</h1>';
+$lsOutput = shell_exec('ls -lh /mnt/');
+echo "<pre>$lsOutput</pre>";
+
 echo "<h1>Show mounts inside of container:</h1>";
 $output = shell_exec('mount');
 echo "<pre>$output</pre>";
