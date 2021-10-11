5edc08dfd4cf86b9a011926a83f4d75303f36cb6
Aleksandr Prikhozhdenko <aprikhozhdenko@plesk.com>
2021-10-11 22:35:04 +0700

    version 1

--- src/index.php
+++ src/index.php
@@ -3,8 +3,6 @@
 echo "Hello, World from Docker ruuning at ECS!<br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png"><br>';
 
-while (list($var,$value) = each ($_ENV)) {
-    echo "$var => $value <br />";
-}
+echo "<h1>Version 1</h1>"
 
 ?>
