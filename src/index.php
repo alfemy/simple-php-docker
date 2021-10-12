0d960dd699514deabf831c3bd9a261ada516969d
Aleksandr Prikhozhdenko <aprikhozhdenko@plesk.com>
2021-10-12 12:25:03 +0700

    version 4

--- src/index.php
+++ src/index.php
@@ -3,6 +3,6 @@
 echo "Hello, World from Docker ruuning at ECS!<br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png"><br>';
 
-echo "<h1>Version 3</h1>"
+echo "<h1>Version 4</h1>"
 
 ?>
